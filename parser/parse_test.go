041e626a1bf36e0969c70712d8c450762ecd6661
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2016-05-24 09:01:29 -0700

    Comments

--- parser/parse_test.go
+++ parser/parse_test.go
@@ -11,7 +11,15 @@ import (
 const (
 	minimal = `package main
 `
+	comment = `// comment
+package main
+`
+	comments = `// comment
+package main
 
+// foo
+// bar
+`
 	hello = `package main
 
 import "fmt"
@@ -30,6 +38,39 @@ func TestParseFile_Minimal(t *testing.T) {
 		Package: token.Pos(1),
 		Name:    &ast.Ident{NamePos: 9, Name: "main"},
 		Scope:   &ast.Scope{Objects: map[string]*ast.Object{}},
+	})
+}
+
+func TestParseFile_Comment(t *testing.T) {
+	fs := token.NewFileSet()
+	file, err := ParseFile(fs, "a.go", comment)
+	defect.Equal(t, err, nil)
+	comment := &ast.Comment{Slash: 1, Text: "// comment"}
+	defect.DeepEqual(t, *file.File, ast.File{
+		Doc:      &ast.CommentGroup{List: []*ast.Comment{comment}},
+		Package:  token.Pos(12),
+		Name:     &ast.Ident{NamePos: 20, Name: "main"},
+		Scope:    &ast.Scope{Objects: map[string]*ast.Object{}},
+		Comments: []*ast.CommentGroup{{List: []*ast.Comment{comment}}},
+	})
+}
+
+func TestParseFile_Comments(t *testing.T) {
+	fs := token.NewFileSet()
+	file, err := ParseFile(fs, "a.go", comments)
+	defect.Equal(t, err, nil)
+	top := &ast.CommentGroup{List: []*ast.Comment{{Slash: 1, Text: "// comment"}}}
+	next := &ast.CommentGroup{List: []*ast.Comment{
+		{Slash: 26, Text: "// foo"},
+		{Slash: 33, Text: "// bar"},
+	}}
+
+	defect.DeepEqual(t, *file.File, ast.File{
+		Doc:      top,
+		Package:  token.Pos(12),
+		Name:     &ast.Ident{NamePos: 20, Name: "main"},
+		Scope:    &ast.Scope{Objects: map[string]*ast.Object{}},
+		Comments: []*ast.CommentGroup{top, next},
 	})
 }
 
