51d6675f9b722004d306850b3464912bab53e2a9
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2016-05-24 09:11:06 -0700

    Hello

--- parser/parse_test.go
+++ parser/parse_test.go
@@ -76,6 +76,66 @@ func TestParseFile_Comments(t *testing.T) {
 
 func TestParseFile_Hello(t *testing.T) {
 	fs := token.NewFileSet()
-	_, err := ParseFile(fs, "a.go", hello)
+	file, err := ParseFile(fs, "a.go", hello)
 	defect.Equal(t, err, nil)
+
+	expectedDecl := &ast.BlockStmt{
+		Lbrace: 41,
+		List: []ast.Stmt{
+			&ast.ExprStmt{
+				X: &ast.CallExpr{
+					Fun: &ast.SelectorExpr{
+						X:   &ast.Ident{NamePos: 44, Name: "fmt"},
+						Sel: &ast.Ident{NamePos: 48, Name: "Println"},
+					},
+					Lparen: 55,
+					Args: []ast.Expr{&ast.BasicLit{
+						ValuePos: 56,
+						Kind:     token.STRING,
+						Value:    `"hello world"`,
+					}},
+					Rparen: 69,
+				},
+			},
+		},
+		Rbrace: 71,
+	}
+
+	obj := &ast.Object{
+		Kind: ast.Fun,
+		Name: "main",
+		Decl: expectedDecl,
+	}
+
+	fnDecl := &ast.FuncDecl{
+		Name: &ast.Ident{Name: "main", NamePos: 34, Obj: obj},
+		Type: &ast.FuncType{
+			Func:   29,
+			Params: &ast.FieldList{Opening: 38, Closing: 39},
+		},
+		Body: expectedDecl,
+	}
+
+	obj.Decl = fnDecl
+
+	importSpec := &ast.ImportSpec{Path: &ast.BasicLit{
+		ValuePos: 22,
+		Kind:     token.STRING,
+		Value:    `"fmt"`,
+	}}
+
+	genDecl := &ast.GenDecl{
+		TokPos: 15,
+		Tok:    token.IMPORT,
+		Specs:  []ast.Spec{importSpec},
+	}
+
+	defect.DeepEqual(t, file.File, &ast.File{
+		Package:    1,
+		Name:       &ast.Ident{NamePos: 9, Name: "main"},
+		Decls:      []ast.Decl{genDecl, fnDecl},
+		Scope:      &ast.Scope{Objects: map[string]*ast.Object{"main": obj}},
+		Imports:    []*ast.ImportSpec{importSpec},
+		Unresolved: []*ast.Ident{{NamePos: 44, Name: "fmt"}},
+	})
 }
